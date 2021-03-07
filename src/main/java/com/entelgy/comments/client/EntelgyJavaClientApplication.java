7e12a804661b6fb5fab85231eeffdf41ba3668a1
Franklyn <franklynvidal@outlook.com>
2021-03-07 09:39:23 -0500

    spring initializr base code Spring Boot

---
+++ src/main/java/com/entelgy/comments/client/EntelgyJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.comments.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntelgyJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelgy/comments/client/EntelgyJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelgy.comments.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntelgyJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
